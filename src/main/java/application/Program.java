1a91897b05b92f1668c89c35d8f8edc92ae68522
vonvit <sandrineviturn@gmail.com>
2026-05-20 15:24:50 -0300

    First class: Position

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,8 +1,11 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     void main(){
 
-        System.out.println("Hello world");
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
 }
