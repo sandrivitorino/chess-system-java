6ed72d90d0aab4327a9762af384ecaf11b97a504
vonvit <sandrineviturn@gmail.com>
2026-06-10 18:24:14 -0300

    Checkmate logic

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -11,7 +11,7 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
-        while (true){
+        while (!chessMatch.getCheckMate()) {
             try {
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
@@ -41,7 +41,8 @@ public class Program {
                 System.out.println(e.getMessage());
                 sc.nextLine();
             }
-
         }
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
     }
 }
