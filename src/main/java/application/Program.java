266008bd184ff173609b46874a5c8225295a7abe
vonvit <sandrineviturn@gmail.com>
2026-07-06 18:52:36 -0300

    Fix: better treatment to promotion

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,11 +1,18 @@
 package application;
 
-import chess.*;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
 
-import java.util.*;
+import chess.ChessException;
+import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
-    void main(){
+
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
@@ -19,31 +26,34 @@ public class Program {
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
 
-                boolean [][] possibleMoves = chessMatch.possibleMoves(source);
+                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
                 UI.clearScreen();
                 UI.printBoard(chessMatch.getPieces(), possibleMoves);
-
                 System.out.println();
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
 
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 
-                if (capturedPiece != null){
+                if (capturedPiece != null) {
                     captured.add(capturedPiece);
                 }
 
                 if (chessMatch.getPromoted() != null) {
                     System.out.print("Enter piece for promotion (B/N/R/Q): ");
-                    String type = sc.nextLine();
+                    String type = sc.nextLine().toUpperCase();
+                    while (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
+                        System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+                        type = sc.nextLine().toUpperCase();
+                    }
                     chessMatch.replacePromotedPiece(type);
                 }
             }
-            catch(ChessException e){
+            catch (ChessException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
             }
-            catch(InputMismatchException e){
+            catch (InputMismatchException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
             }
